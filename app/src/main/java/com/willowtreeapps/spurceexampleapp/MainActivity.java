bff70088230b97d5229066d2b723842735bc39a5
bret-fears <bret.fears@willowtreeapps.com>
2017-03-02 10:28:27 -0500

    Example App module
    Add example app foundation
    Add Spruce colors
    Add Spruce icon
    
    Add Spruce library as a dependency

--- app/src/main/java/com/willowtreeapps/spurceexampleapp/MainActivity.java
+++ app/src/main/java/com/willowtreeapps/spurceexampleapp/MainActivity.java
@@ -20,30 +20,16 @@
  *
  */
 
-apply plugin: 'com.android.library'
+package com.willowtreeapps.spurceexampleapp;
 
-android {
-    compileSdkVersion 25
-    buildToolsVersion "25.0.2"
-    defaultConfig {
-        minSdkVersion 16
-        targetSdkVersion 25
-        versionCode 1
-        versionName "1.0"
-        testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner"
-    }
-    buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'
-        }
-    }
-}
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-dependencies {
-    androidTestCompile('com.android.support.test.espresso:espresso-core:2.2.2', {
-        exclude group: 'com.android.support', module: 'support-annotations'
-    })
-    compile 'com.android.support:appcompat-v7:25.2.0'
-    testCompile 'junit:junit:4.12'
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
