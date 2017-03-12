0f15602cc3019eb334f87a0ca9d5eec5b612e2e7
bret-fears <bret.fears@willowtreeapps.com>
2017-03-12 13:38:39 -0400

    Port RandomSort
    - Add RandomSort

---
+++ lib/src/main/java/com/willowtreeapps/spruce/sort/RandomSort.java
@@ -0,0 +1,62 @@
+/*
+ *     Spruce
+ *
+ *     Copyright (c) 2017 WillowTree, Inc.
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
+ *     The above copyright notice and this permission notice shall be included in
+ *     all copies or substantial portions of the Software.
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *     THE SOFTWARE.
+ *
+ */
+
+package com.willowtreeapps.spruce.sort;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class RandomSort extends SortFunction {
+
+    private final long interObjectDelay;
+
+    /**
+     * Random sort pattern that utilizes {@link Collections#shuffle(List, Random) shuffle()}
+     *
+     * @param interObjectDelay
+     */
+    public RandomSort(long interObjectDelay) {
+        this.interObjectDelay = interObjectDelay;
+    }
+
+    @Override
+    public List<SpruceTimedView> getViewListWithTimeOffsets(ViewGroup parent, List<View> children) {
+        // randomize view list
+        Collections.shuffle(children, new Random(System.nanoTime()));
+
+        List<SpruceTimedView> timedViews = new ArrayList<>();
+        long currentTimeOffset = 0;
+
+        for (View view : children) {
+            timedViews.add(new SpruceTimedView(view, currentTimeOffset));
+            currentTimeOffset += interObjectDelay;
+        }
+
+        return timedViews;
+    }
+
+}
