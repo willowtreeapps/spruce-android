5618e8496b02674ef96b9dda13fcdf65394e0c9a
teddywilson <teddy@roof.io>
2017-07-11 10:57:46 -0400

    added logo, inverted background

--- app/src/main/java/com/willowtreeapps/spurceexampleapp/SplashActivity.java
+++ app/src/main/java/com/willowtreeapps/spurceexampleapp/SplashActivity.java
@@ -26,16 +26,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_TIMEOUT = 2000;
+    private ImageView logoImageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logoImageView = (ImageView) findViewById(R.id.ivLogo);
+        logoImageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.spruce_logo));
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -45,6 +51,7 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIMEOUT);
+
     }
 
 }
